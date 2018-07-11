19cd0365ba8e1d3b304f53b5253878f432e90109
Seraethy <seraethy@gmail.com>
2018-07-12 00:07:56 +0200

    Revamped the moderation cog

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -4,9 +4,6 @@ import discord
 from discord.ext import commands
 
 
-MUTED_ROLE = 446371354615218186
-
-
 class MemberID(commands.Converter):
     async def convert(self, ctx, argument):
         try:
@@ -15,7 +12,7 @@ class MemberID(commands.Converter):
             try:
                 return int(argument, base=10)
             except ValueError:
-                raise commands.BadArgument(f"{argument} is not a valid member or member ID.") from None
+                raise commands.BadArgument(f'{argument} is not a valid member or member ID.') from None
         else:
             can_execute = ctx.author.id == ctx.bot.owner_id or \
                           ctx.author == ctx.guild.owner or \
@@ -36,147 +33,241 @@ class BannedMember(commands.Converter):
             entity = discord.utils.find(lambda u: str(u.user) == argument, ban_list)
 
         if entity is None:
-            raise commands.BadArgument("Not a valid previously-banned member.")
+            raise commands.BadArgument('Not a valid previously-banned member.')
         return entity
 
 
 class Moderation:
 
+
     def __init__(self, bot):
         self.bot = bot
-        self.timers = bot.get_cog("Timers")
-        self.stafflog = bot.get_cog("Stafflog")
-        
+        self.stafflog = bot.get_cog('Stafflog')
+        self.timers = bot.get_cog('Timers')
+
+
     @commands.command()
-    @commands.has_any_role("Administrator", "Moderator", "Helper")
-    async def kick(self, ctx, member: MemberID, *, reason=None):
-        if reason is None:
-            reason = "The author didn't provide a reason"
-        if ctx:
+    @commands.has_any_role('Administrator', 'Moderator', 'Helper')
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        if discord.utils.get(member.roles, id=437969862573424670):
+            em = discord.Embed(description='You can\'t kick a staff member')
+            em.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+            await ctx.send(embed=em, delete_after=6)
+            return
+
+        elif reason is None:
+            reason = 'The author didn\'t provide a reason'
+        
+        elif ctx: 
             try:
-                user = await self.bot.get_user_info(member)
-                em = discord.Embed(description=f"{user} got kicked!")
-                em.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-                await ctx.send(embed=em, delete_after=6)
-                emb = discord.Embed(description=f"Action: Kick\nReason: {reason}")
-                emb.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-                await user.send(embed=emb)
-                await ctx.guild.kick(discord.Object(id=member), reason=reason)
-            except Exception:
-                await ctx.guild.kick(discord.Object(id=member), reason=reason)
+                emb = discord.Embed(description=f'{member} got kicked')
+                emb.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+                await ctx.send(embed=emb, delete_after=6)
+                embe = discord.Embed(description=f'Action: Kick\nReason: {reason}')
+                embe.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+                await member.send(embed=embe)
+            except:
+                pass
+
+        await ctx.guild.kick(discord.Object(id=member.id), reason=reason)
         await self.stafflog.make_case(member, "Kick", reason, ctx.author)
 
+
     @commands.command()
-    @commands.has_any_role("Administrator", "Moderator", "Helper")
+    @commands.has_any_role('Administrator', 'Moderator', 'Helper')
     async def ban(self, ctx, member: MemberID, *, reason=None):
-        if reason is None:
-            reason = "The author didn't provide a reason"
-        if ctx:
+        server = self.bot.get_guild(406872145692590080)
+        user = server.get_member(member)
+        if discord.utils.get(user.roles, id=437969862573424670):
+            em = discord.Embed(description='You can\'t ban a staff member')
+            em.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+            await ctx.send(embed=em, delete_after=6)
+            return
+        
+        elif reason is None:
+            reason = 'The author didn\'t provide a reason'
+
+        elif ctx:
             try:
-                user = await self.bot.get_user_info(member)
-                em = discord.Embed(description=f"{user} got banned!")
-                em.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-                await ctx.send(embed=em, delete_after=6)
-                if user in ctx.guild.members:
-                    emb = discord.Embed(description=f"Action: Ban\nReason: {reason}")
-                    emb.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-                    await user.send(embed=emb)
-                    await ctx.guild.ban(discord.Object(id=member), reason=reason)
-                else:
-                    await ctx.guild.ban(discord.Object(id=member), reason=reason)
-            except Exception:
-                await ctx.guild.ban(discord.Object(id=member), reason=reason)
-        await self.stafflog.make_case(member, "Ban", reason, ctx.author)
+                emb = discord.Embed(description=f'{user} got banned')
+                emb.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+                await ctx.send(embed=emb, delete_after=6)
+                embe = discord.Embed(description=f'Action: Ban\nReason: {reason}')
+                embe.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+                await user.send(embed=embe)
+            except:
+                pass
+
+        await ctx.guild.ban(discord.Object(id=member), reason=reason)
+        await self.stafflog.make_case(user, 'Ban', reason, ctx.author)
+
 
     @commands.command()
-    @commands.has_any_role("Administrator", "Moderator", "Helper")
-    async def softban(self, ctx, member: MemberID, *, reason=None):
-        if reason is None:
-            reason = "The author didn't provide a reason"
-        if ctx:
-            try:
-                user = await self.bot.get_user_info(member)
-                em = discord.Embed(description=f"{user} got softbanned!")
-                em.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-                await ctx.send(embed=em, delete_after=6)
-                if user in ctx.guild.members:
-                    emb = discord.Embed(description=f"Action: Softban\nReason: {reason}")
-                    emb.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-                    await user.send(embed=emb)
-                    await ctx.guild.ban(discord.Object(id=member), reason=reason)
-                    await ctx.guild.unban(discord.Object(id=member), reason=reason)
-                else:
-                    await ctx.guild.ban(discord.Object(id=member), reason=reason)
-                    await ctx.guild.unban(discord.Object(id=member), reason=reason)
-            except Exception:
-                await ctx.guild.ban(discord.Object(id=member), reason=reason)
-                await ctx.guild.unban(discord.Object(id=member), reason=reason)
-        await self.stafflog.make_case(member, "Softban", reason, ctx.author)
+    @commands.has_any_role('Administrator', 'Moderator', 'Helper')
+    async def tempban(self, ctx, member: MemberID, days: int, *, reason=None):
+        server = self.bot.get_guild(406872145692590080)
+        user = server.get_member(member)
+        if discord.utils.get(user.roles, id=437969862573424670):
+            em = discord.Embed(description='You can\'t ban a staff member')
+            em.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+            await ctx.send(embed=em, delete_after=6)
+            return
 
-    @commands.command()
-    @commands.has_any_role("Administrator", "Moderator", "Helper")
-    async def unban(self, ctx, member: BannedMember, *, reason=None):
-        if reason is None:
-            reason = "The author didn't provide a reason"
-        em = discord.Embed(description=f"{member.user} got their ban removed!")
-        em.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-        await ctx.send(embed=em, delete_after=6)
-        await ctx.guild.unban(member.user, reason=reason)
-        await self.stafflog.make_case(member, "Unban", reason, ctx.author)
+        elif reason is None:
+            reason = 'The author didn\'t provide a reason'
 
-    @commands.command()
-    @commands.has_any_role("Administrator", "Moderator", "Helper")
-    async def mute(self, ctx, member: discord.Member, minutes: int = 5, *, reason=None):
-        if reason is None:
-            reason = "The author didn't provide a reason"
-        if ctx:
+        elif ctx:
             try:
-                em = discord.Embed(description=f"{member} got muted!")
-                em.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-                await ctx.send(embed=em, delete_after=6)
-                emb = discord.Embed(description=f'Mute ({minutes} minute{"s" if minutes!=1 else ""})\nReason: {reason}')
-                emb.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-                await member.send(embed=emb)
-                await member.add_roles(discord.utils.get(ctx.guild.roles, id=MUTED_ROLE))
-            except Exception:
-                await member.add_roles(discord.utils.get(ctx.guild.roles, id=MUTED_ROLE))
-        await self.timers.create_timer("unmute", datetime.datetime.utcnow() + datetime.timedelta(minutes=minutes), [ctx.guild.id, member.id])
-        await self.stafflog.make_case(member, f'Mute ({minutes} minute{"s" if minutes!=1 else ""})', reason, ctx.author)
-    
-    async def on_unmute_event(self, guild_id, user_id):
+                emb = discord.Embed(description=f'{user} got banned for {days} day{"s" if days!=1 else ""}')
+                emb.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+                await ctx.send(embed=emb, delete_after=6)
+                embe = discord.Embed(description=f'Action: Temporary ban ({days} day{"s" if days!=1 else ""})\nReason: {reason}')
+                embe.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+                await user.send(embed=embe)                
+            except:
+                pass
+
+        await ctx.guild.ban(discord.Object(id=member), reason=reason)
+        await self.timers.create_timer('unban', datetime.datetime.utcnow() + datetime.timedelta(days=days), [ctx.guild.id, member])
+        await self.stafflog.make_case(user, f'Temporary ban ({days} day{"s" if days!=1 else ""})', reason, ctx.author)
+
+
+    async def on_unban_event(self, guild_id, user_id):
         guild = self.bot.get_guild(guild_id)
         member = guild.get_member(user_id)
         if member:
             try:
-                await member.remove_roles(discord.utils.get(guild.roles, id=MUTED_ROLE))
-            except Exception:
+                await guild.unban(discord.Object(id=member.id), reason='Expiration of temporary ban')
+                await self.stafflog.make_case(member, 'Unban', 'Expiration of temporary ban', 'Harmony#8978')
+            except:
                 return
 
+
     @commands.command()
-    @commands.has_any_role("Administrator", "Moderator", "Helper")
-    async def unmute(self, ctx, member: discord.Member, *, reason=None):
-        if reason is None:
-            reason = "The author didn't provide a reason"
-        if ctx:
+    @commands.has_any_role('Administrator', 'Moderator', 'Helper')
+    async def softban(self, ctx, member: MemberID, *, reason=None):
+        server = self.bot.get_guild(406872145692590080)
+        user = server.get_member(member)
+        if discord.utils.get(user.roles, id=437969862573424670):
+            em = discord.Embed(description='You can\'t ban a staff member')
+            em.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+            await ctx.send(embed=em, delete_after=6)
+            return
+
+        elif reason is None:
+            reason = 'The author didn\'t provide a reason'
+
+        elif ctx:
             try:
-                em = discord.Embed(description=f"{member} got their mute removed!")
-                em.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-                await ctx.send(embed=em, delete_after=6)
-                emb = discord.Embed(description=f"Unmute\nReason: {reason}")
-                emb.set_author(name=f"{ctx.author} ({ctx.author.id})", icon_url=f"{ctx.author.avatar_url}")
-                await member.send(embed=emb)
-                await member.remove_roles(discord.utils.get(ctx.guild.roles, id=MUTED_ROLE))
-            except Exception:
-                await member.remove_roles(discord.utils.get(ctx.guild.roles, id=MUTED_ROLE))
-        await self.stafflog.make_case(member, "Unmute", reason, ctx.author)
+                emb = discord.Embed(description=f'{user} got softbanned')
+                emb.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+                await ctx.send(embed=emb, delete_after=6)
+                embe = discord.Embed(description=f'Action: Softban\nReason: {reason}')
+                embe.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+                await user.send(embed=embe)                
+            except:
+                pass
+
+            await ctx.guild.ban(discord.Object(id=member), reason=reason)
+            await ctx.guild.unban(discord.Object(id=member), reason=reason)
+            await self.stafflog.make_case(member, 'Softban', reason, ctx.author)
+        
+
+    @commands.command()
+    @commands.has_any_role('Administrator', 'Moderator', 'Helper')
+    async def unban(self, ctx, member: BannedMember, *, reason=None):
+        if reason is None:
+            reason = 'The author didn\'t provide a reason'
+
+        elif ctx:
+            try:
+                emb = discord.Embed(description=f'{member.user} got their ban removed')
+                emb.set_author(name=f'{ctx.author} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+                await ctx.send(embed=emb, delete_after=6)              
+            except:
+                pass
+
+        await ctx.guild.unban(member.user, reason=reason)
+        await self.stafflog.make_case(member.user, 'Unban', reason, ctx.author)
 
 
-#ADD CLEAR/PRUNE COMMAND
-#CLEAN UP THE CODE EVENTUALLY
-#ADD A WARN COMMAND
-#MAYBE ADD INVOCATION DELETION (ctx.message.delete())
-#A TEMPORARY BAN WITH TIMER AND SHIT
-#MAKE IT SO YOU CAN'T USE COMMANDS ON OTHER STAFF
+    @commands.command()
+    @commands.has_any_role('Administrator', 'Moderator', 'Helper')
+    async def mute(self, ctx, member: discord.Member, minutes: int, *, reason=None):
+        if discord.utils.get(member.roles, id=437969862573424670):
+            em = discord.Embed(description='You can\'t mute a staff member')
+            em.set_author(name=f'{ctx.author}', icon_url=f'{ctx.author.avatar_url}')
+            await ctx.send(embed=em, delete_after=6)
+            return
+        
+        elif discord.utils.get(member.roles, id=446371354615218186):
+            emb = discord.Embed(description='You can\'t mute a member twice')
+            emb.set_author(name=f'{ctx.author}', icon_url=f'{ctx.author.avatar_url}')
+            await ctx.send(embed=emb, delete_after=6)
+            return
+
+        elif reason is None:
+            reason = 'The author didn\'t provide a reason'
+
+        elif ctx:
+            try:
+                embe = discord.Embed(description=f'{member} got muted')
+                embe.set_author(name=f'{ctx.author}', icon_url=f'{ctx.author.avatar_url}')
+                await ctx.send(embed=embe, delete_after=6)
+                embed = discord.Embed(description=f'Action: Mute ({minutes} minute{"s" if minutes!=1 else ""})\nReason: {reason}')
+                embed.set_author(name=f'{ctx.author}', icon_url=f'{ctx.author.avatar_url}')
+                await member.send(embed=embed)
+            except:
+                pass
+        
+        await member.add_roles(discord.utils.get(ctx.guild.roles, id=446371354615218186))        
+        await self.timers.create_timer('unmute', datetime.datetime.utcnow() + datetime.timedelta(minutes=minutes), [ctx.guild.id, member.id])
+        await self.stafflog.make_case(member, f'Mute ({minutes} minute{"s" if minutes!=1 else ""})', reason, ctx.author)
+
+
+    async def on_unmute_event(self, guild_id, user_id):
+        #NEEDS WORK
+        guild = self.bot.get_guild(guild_id)
+        member = guild.get_member(user_id)
+        if member:
+            try:
+                #EMBED FOR GETTING UNMUTED
+                #LOG UNMUTE
+                await member.remove_roles(discord.utils.get(guild.roles, id=446371354615218186))
+            except:
+                return
+
+
+    @commands.command()
+    @commands.has_any_role('Administrator', 'Moderator', 'Helper')
+    async def unmute(self, ctx, member: discord.Member, *, reason=None):
+        if discord.utils.get(member.roles, id=446371354615218186) is None:
+            em = discord.Embed(description='This person isn\'t muted')
+            em.set_author(name=f'{ctx.author}', icon_url=f'{ctx.author.avatar_url}')
+            await ctx.send(embed=em, delete_after=6)
+            return
+
+        elif reason is None:
+            reason = 'The author didn\'t provide a reason'
+
+        elif ctx:
+            try:
+                emb = discord.Embed(description=f'**{member.name}** got their mute removed')
+                emb.set_author(name=f'{ctx.author}', icon_url=f'{ctx.author.avatar_url}')
+                await ctx.send(embed=emb, delete_after=6)
+                embe = discord.Embed(description=f'Action: Unmute\nReason: {reason}')
+                embe.set_author(name=f'{ctx.author}', icon_url=f'{ctx.author.avatar_url}')
+                await member.send(embed=embe)
+            except:
+                pass
+
+        await member.remove_roles(discord.utils.get(ctx.guild.roles, id=446371354615218186))
+        await self.stafflog.make_case(member, 'Unmute', reason, ctx.author)
+
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
+
+#COLOR EMBEDS?
+#CLEAR/PRUNE COMMANDS
+#WARN COMMANDS
